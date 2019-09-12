4c8cd5a27710d6220d6d6ac2f8f623f11bfbf460
Manish Roy <manish.roy@utah.edu>
2019-09-11 18:18:57 -0600

    Split visualizations for right and left saccade

--- parent_heatmap_js_right.js
+++ parent_heatmap_js_right.js
@@ -1,57 +1,39 @@
-
-function assign_colors(d){
-    if (d.cell_id_x == fp1_x && d.cell_id_y == fp1_y) {
-        return "cell_rf1";
-    }
-    else if (d.cell_id_x == fp2_x && d.cell_id_y == fp2_y) {
-        return "cell_rf2";
-    }
-    else
-    {
-        return "cell_initial";
-    }
-}
-
-// #################################################################################################
-// ############################ Compute ct #########################################################
-
+let ct_array_rt = new Array();
 
 function compute_ct(d){
-    console.log('-----------------------');
-    let rf1pf1 = Math.sqrt(Math.pow((d.cell_id_x-fp1_x),2) +  Math.pow((d.cell_id_y-fp1_y),2));
-    let rf1pf2 = Math.sqrt(Math.pow((d.cell_id_x-fp2_x),2) +  Math.pow((d.cell_id_y-fp2_y),2));
-    console.log('RF1FP1: '+ rf1pf1 + ' AND ' + 'RF1FP2: '+ rf1pf2);
-
+    //console.log('-----------------------');
+    let rf1pf1 = Math.sqrt(Math.pow((d.cell_id_x-fp1_x_rt),2) +  Math.pow((d.cell_id_y-fp1_y_rt),2));
+    let rf1pf2 = Math.sqrt(Math.pow((d.cell_id_x-fp2_x_rt),2) +  Math.pow((d.cell_id_y-fp2_y_rt),2));
+    //console.log('RF1FP1: '+ rf1pf1 + ' AND ' + 'RF1FP2: '+ rf1pf2);
     let ind = (rf1pf2 - rf1pf1)/(rf1pf1 + rf1pf2);
-    console.log('ind-->'+ind);
-
+    //console.log('ind-->'+ind);
     // Setting saccade direction :
     let sacc;
-    if (fp2_x < fp1_x){
+    if (fp2_x_rt < fp1_x_rt){
         sacc = -1;
-        console.log('Left Saccade  sacc='+sacc);
+        //console.log('Left Saccade  sacc='+sacc);
     }
     else{
         sacc = 1;
-        console.log('Right Saccade  sacc='+sacc);
+        //console.log('Right Saccade  sacc='+sacc);
     }
-
     let hem;
     // setting hemifield value
-    if (d.cell_id_x >= fp1_x){
+    if (d.cell_id_x >= fp1_x_rt){
         // RF of neuron is to the right of the FP1 and hence left hemisphere = -1 (Right Hemifield)
         // (= 0 for compuational purpose)
-        hem = 0;
-        console.log('Right Hemifield   hem ='+hem);
+        hem = -1;
+        //console.log('Right Hemifield   hem ='+hem);
     }
     else{
         hem = 1;
-        console.log('Left Hemifield   hem ='+hem);
+        //console.log('Left Hemifield   hem ='+hem);
     }
-
     let st_hem = sacc * hem;
-    console.log('st_hem-->' + st_hem );
-
+    if (st_hem == -1){
+        st_hem = 0;
+    }
+    //console.log('st_hem-->' + st_hem );
     // Computing ct using the developed model
     let x1 = 1/rf1pf1;
     let x2 = 1/rf1pf2;
@@ -68,23 +50,54 @@ function compute_ct(d){
     let temp_delay_mod = 5 + -51.7 * x1 - 11 * x2 + 3.6 * x3 -5 * x4 + 188 * x1x2
         -7.58 * x1x4 - 9.5 * x2x3 + 68 * x2x4 + 4.8 * x3x4 + 61 * x1x1 - 18 * x2x2;
 
+    ct_array_rt.push(temp_delay_mod);
     return temp_delay_mod;
 }
 
+
 // FP1 & FP2
-let fp1_x = 0;
-let fp1_y = 0;
-let fp2_x = -1;
-let fp2_y = 0;
+let fp1_x_rt = 0;
+let fp1_y_rt = 0;
+let fp2_x_rt = 10;
+let fp2_y_rt = 0;
+
+// function assign_colors(d){
+//     if (d.cell_id_x == fp1_x_rt && d.cell_id_y == fp1_y_rt) {
+//         return "cell_rf1";
+//     }
+//     else if (d.cell_id_x == fp2_x_rt && d.cell_id_y == fp2_y_rt) {
+//         return "cell_rf2";
+//     }
+//     else
+//     {
+//         return "cell_others";
+//     }
+// }
+
 
 // create a tooltip
-var Tooltip = d3.select("#grid")
+var Tooltip = d3.select("#grid_right")
     .append("div")
     .style("opacity", 0)
     .attr("class", "tooltip")
     .style("background-color", "white")
 
 function mouseover(d){
+    //console.log('ct_array_rt-->'+ct_array_rt);
+    let min = ct_array_rt[0];
+    let max = ct_array_rt[0];
+    for (i =0; i<ct_array_rt.length; i++){
+        if (ct_array_rt[i]>max){
+            max = ct_array_rt[i];
+        }
+        if (ct_array_rt[i]<min){
+            min = ct_array_rt[i];
+        }
+        //console.log(ct_array_rt[i]);
+    }
+    //console.log('max-->'+max);
+    //console.log('min-->'+min);
+
     d3.select(this).attr("class","cell_hover");
     Tooltip
         .style("opacity", 1)
@@ -92,19 +105,31 @@ function mouseover(d){
 
 function mousemove (d) {
     Tooltip
-        .html("x,y : " + d.cell_id_x + ',' +d.cell_id_y)
+        .html("<b>(x,y) = (" + d.cell_id_x + ',' +d.cell_id_y + ') and  ct = '+ d.ct )
         .style("left", (d3.mouse(this)[0]+70) + "px")
         .style("top", (d3.mouse(this)[1]) + "px")
 }
 
 function mouseleave(d) {
-    d3.select(this).attr("class", assign_colors(d));
+    d3.select(this).attr("class", "cell");
     Tooltip
         .style("opacity", 0);
 }
 
-// #################################################################################################
-// ############################ GRID and maipulation of squares ####################################
+
+
+// Build color scale
+var myColor = d3.scaleLinear()
+    .domain([-6,36])
+    .range(["#C9FBFF", "#030A97"])
+
+
+
+
+// X --> columns
+var x_range = 25;
+// Y --> rows
+var y_range = 7;
 
 function gridData() {
     var data = new Array();
@@ -115,13 +140,10 @@ function gridData() {
 
     var click = false;
     var cell_cntr = 0;
-    // X --> columns
-    var x_range = 3
-    // Y --> rows
-    var y_range = 3
+
     var x_mid = Math.ceil(x_range/2);
     var y_mid = Math.ceil(y_range/2);
-
+    let ct = 88;
 
     // iterate for rows
     for (var row = 0; row < y_range; row++) {
@@ -132,12 +154,12 @@ function gridData() {
             data[row].push({
                 x: xpos,
                 y: ypos,
-                cell_val : cell_cntr,
                 cell_id_x : column + 1 - x_mid,
                 cell_id_y : (row + 1 - y_mid)*(-1),
                 width: width,
                 height: height,
-                click: click
+                click: click,
+                ct : ct
             })
             // increment the x position. I.e. move it over by 50 (width variable)
             xpos += width;
@@ -150,16 +172,19 @@ function gridData() {
     return data;
 }
 
-
-
 var gridData = gridData();
 console.log(gridData);
 
-var grid = d3.select("#grid")
+let svg_width_rt = 50*(x_range+1);
+let svg_height_rt = 50*(y_range+1);
+
+
+
+var grid = d3.select("#grid_right")
     .append("svg")
     .attr("id", "svg1")
-    .attr("width","1100px")
-    .attr("height","1100px");
+    .attr("width",svg_width_rt)
+    .attr("height",svg_height_rt);
 
 var row = grid.selectAll(".row")
     .data(gridData)
@@ -168,42 +193,35 @@ var row = grid.selectAll(".row")
 
 var column = row.selectAll(".square")
     .data(function(d) {
-       // console.log('d--' ,d);
+        // console.log('d--' ,d);
         return d; })
     .enter().append("rect")
     .attr("class","square")
     .attr("x", function(d) { return d.x; })
     .attr("y", function(d) { return d.y; })
-    .attr("cell_val", function(d) { return d.cell_val; })
     .attr("cell_id_x", function(d) { return d.cell_id_x; })
     .attr("cell_id_y", function(d) { return d.cell_id_y; })
     .attr("width", function(d) { return d.width; })
     .attr("height", function(d) { return d.height; })
-    .attr("class", function(d){
-        if (d.cell_id_x == fp1_x && d.cell_id_y == fp1_y) {
-            return "cell_rf1";
-        }
-        else if (d.cell_id_x == fp2_x && d.cell_id_y == fp2_y) {
-            return "cell_rf2";
-        }
-        else
-        {
-            return "cell_initial";
-        }
+    .attr("class", "cell")
+    .attr("ct",function (d) {
+        d.ct = compute_ct(d);
+        //console.log('ct===='+d.ct);
+        return d.ct;
     })
-    .on('click', function(d) {
-        let ct = compute_ct(d);
-        console.log('ct-->'+ct);
-
+    .style("fill", function(d) {
+        //console.log('location-->'+d.cell_id_x+', '+d.cell_id_y);
+        //console.log('myColor--->'+myColor(d.ct));
+        if (d.cell_id_x == fp1_x_rt && d.cell_id_y == fp1_y_rt){
+            return "#E3D11E"
+        }
+        else if (d.cell_id_x == fp2_x_rt && d.cell_id_y == fp2_y_rt){
+            return "#DA2E00"
+        }
+        else{
+            return myColor(d.ct);
+        }
     })
     .on("mouseover", mouseover)
     .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
-;
-
-
-
-
-
-
-
+    .on("mouseleave", mouseleave);
